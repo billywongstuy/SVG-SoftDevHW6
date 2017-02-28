072e885af96a16e3f5519099660c6f3247d7c91d
Billy Wong <bwong5@stuy.edu>
2017-02-28 13:58:05 -0500

    cleaner

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
+/*------------------
+ VARIABLES
+------------------*/
+
 var pic = document.getElementById("vimage");
+var clearB = document.getElementById("clear");
 var prevX = null;
 var prevY = null;
 
+/*------------------------------------------
+ ADDS CIRCLE AND LINE WHEN CLICK ON SVG
+------------------------------------------*/
+
 var addCircle = function(e) {
-    var x = e.offsetX;
-    var y = e.offsetY;
-
     var c = document.createElementNS("http://www.w3.org/2000/svg","circle");
-
     c.setAttribute("r","20");
     c.setAttribute("fill","rgba(255,0,0,0.5)");
     c.setAttribute("stroke","black");
-    
-    c.setAttribute("cx",x);
-    c.setAttribute("cy",y);
-    
+    c.setAttribute("cx",e.offsetX);
+    c.setAttribute("cy",e.offsetY);
     pic.appendChild(c);
 };
 
 
-
 var addLine = function(e) {
     var l = document.createElementNS("http://www.w3.org/2000/svg","line");
+    l.setAttribute("stroke","black");
+    l.setAttribute("stroke-width",1);
     l.setAttribute("x1",prevX);
     l.setAttribute("y1",prevY);
     l.setAttribute("x2",e.offsetX);
     l.setAttribute("y2",e.offsetY);
-    l.setAttribute("stroke","black");
-    l.setAttribute("stroke-width",1);
-    
     pic.appendChild(l);
-    
 };
 
 
@@ -47,12 +47,13 @@ var addStuff = function(e) {
 pic.addEventListener("click",addStuff);
 
 
-var clearB = document.getElementById("clear");
+/*--------------
+ CLEAR
+--------------*/
+
 var clear = function(e) {
     while (pic.lastChild) {
 	pic.removeChild(pic.lastChild);
     }
 };
-
-
 clearB.addEventListener("click",clear);
